9cc8874bf891e19d39383f9fdf7e93249a552d6e
Lawrence Matsuyama <law.matsuyama@outlook.com>
2022-12-07 18:58:26 -0300

    chore: added cleanup function for wait group

--- domain/service_wait_group.go
+++ domain/service_wait_group.go
@@ -1,17 +1,26 @@
 package domain
 
-import "sync"
+import (
+	"fmt"
+	"sync"
+)
 
 var wg = &sync.WaitGroup{}
 
 func AddTaskCount() {
+	fmt.Printf("add task\n")
 	wg.Add(1)
 }
 
 func DoneTask() {
+	fmt.Printf("done task\n")
 	wg.Done()
 }
 
 func WaitUntilAllTasksDone() {
 	wg.Wait()
+}
+
+func CleanupWaitGroup() {
+	wg = new(sync.WaitGroup)
 }
